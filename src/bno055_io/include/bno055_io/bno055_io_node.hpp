d52ffea713fa76bd5b7ddf71e32c9f0df3534dff
ryuzou <ryuzot.1112@gmail.com>
2022-04-27 18:16:58 +0900

    bno055用nodeの更新 ガワは代替できた 共分散等の算出はまだ

--- src/bno055_io/include/bno055_io/bno055_io_node.hpp
+++ src/bno055_io/include/bno055_io/bno055_io_node.hpp
@@ -2,13 +2,57 @@
 #define BNO055_IO__BNO055_IO_NODE_HPP_
 
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 
 #include "bno055_io/visibility_control.h"
+#include "socketcan_interface_msg/msg/socketcan_if.hpp"
 #include "visibility_control.h"
+
+#define AccelX_CanId 0x0312
+#define AccelY_CanId 0x0313
+#define AccelZ_CanId 0x0314
+
+#define GyroX_CanId 0x0322
+#define GyroY_CanId 0x0323
+#define GyroZ_CanId 0x0324
+
+#define QuatX_CanId 0x0332
+#define QuatY_CanId 0x0333
+#define QuatZ_CanId 0x0334
+
 
 namespace bno055_io {
 
     class Bno055Io : public rclcpp::Node {
+    private:
+
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_AccelX;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_AccelY;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_AccelZ;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_GyroX;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_GyroY;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_GyroZ;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_QuatX;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_QuatY;
+        rclcpp::Subscription<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _subscriber_QuatZ;
+
+        rclcpp::TimerBase::SharedPtr _pub_timer;
+
+        rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr bno055_imu_data;
+        rclcpp::QoS _qos = rclcpp::QoS(1).best_effort();
+
+        void _publisher_callback();
+
+        void _subscriber_callback_AccelX(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_AccelY(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_AccelZ(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_GyroX(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_GyroY(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_GyroZ(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_QuatX(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_QuatY(socketcan_interface_msg::msg::SocketcanIF msg);
+        void _subscriber_callback_QuatZ(socketcan_interface_msg::msg::SocketcanIF msg);
+
     public:
         BNO055_IO_PUBLIC
         explicit Bno055Io(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
--- src/bno055_io/src/bno055_io_node.cpp
+++ src/bno055_io/src/bno055_io_node.cpp
@@ -1,3 +1,10 @@
+//
+// Created by ryuzo on 2022/04/15.
+//
+#include <rclcpp/rclcpp.hpp>
+#include <chrono>
+
+#include "socketcan_interface_msg/msg/socketcan_if.hpp"
 #include "bno055_io/bno055_io_node.hpp"
 
 namespace bno055_io {
@@ -5,7 +12,95 @@ namespace bno055_io {
     Bno055Io::Bno055Io(const rclcpp::NodeOptions &options) : Bno055Io("", options){}
 
     Bno055Io::Bno055Io(const std::string &name_space, const rclcpp::NodeOptions &options) : rclcpp::Node(
-            "socket_can_node", options) {
+            "bno055_node", options) {
+        using namespace std::chrono_literals;
+
+        declare_parameter("interval_ms", 50);
+        auto interval_ms = this->get_parameter("interval_ms").as_int();
+
+        _pub_timer = this->create_wall_timer(
+                std::chrono::milliseconds(interval_ms),
+                [this] { _publisher_callback(); }
+        );
+        
+        _subscriber_AccelX = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(AccelX_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_AccelX, this, std::placeholders::_1));
+        _subscriber_AccelY = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(AccelY_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_AccelY, this, std::placeholders::_1));
+        _subscriber_AccelZ = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(AccelZ_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_AccelZ, this, std::placeholders::_1));
+
+        _subscriber_GyroX = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(GyroX_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_GyroX, this, std::placeholders::_1));
+        _subscriber_GyroY = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(GyroY_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_GyroY, this, std::placeholders::_1));
+        _subscriber_GyroZ = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(GyroZ_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_GyroZ, this, std::placeholders::_1));
+
+        _subscriber_QuatX = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(QuatX_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_QuatX, this, std::placeholders::_1));
+        _subscriber_QuatY = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(QuatY_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_QuatY, this, std::placeholders::_1));
+        _subscriber_QuatZ = this->create_subscription<socketcan_interface_msg::msg::SocketcanIF>(
+                std::string("can_rx_" + std::to_string(QuatZ_CanId)),
+                _qos,
+                std::bind(&Bno055Io::_subscriber_callback_QuatZ, this, std::placeholders::_1));
+    }
+
+    void Bno055Io::_publisher_callback() {
+
+    }
+
+    void Bno055Io::_subscriber_callback_AccelX(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_AccelY(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_AccelZ(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_GyroX(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_GyroY(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_GyroZ(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_QuatX(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_QuatY(socketcan_interface_msg::msg::SocketcanIF msg) {
+
+    }
+
+    void Bno055Io::_subscriber_callback_QuatZ(socketcan_interface_msg::msg::SocketcanIF msg) {
+
     }
 
 
