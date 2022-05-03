d416e91ff6a5f2d905623ca68884f08c81d31122
ryuzou <ryuzot.1112@gmail.com>
2022-05-03 19:52:57 +0900

    tcpからの通信をnon-blockingでstd_msgのstring型でpublishする

--- src/tcp_joystick_handler/include/tcp_joystick_handler/tcp_joystick_handler_node.hpp
+++ src/tcp_joystick_handler/include/tcp_joystick_handler/tcp_joystick_handler_node.hpp
@@ -1,18 +1,40 @@
 #ifndef TCP_JOYSTICK_HANDLER__TCP_JOYSTICK_HANDLER_NODE_HPP_
 #define TCP_JOYSTICK_HANDLER__TCP_JOYSTICK_HANDLER_NODE_HPP_
 
-#include "tcp_joystick_handler/visibility_control.h"
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <chrono>
 
-namespace tcp_joystick_handler
-{
+#include "visibility_control.h"
+#include "tcp_nonblocking.hpp"
 
-class TcpJoystickHandlerNode
-{
-public:
-  TcpJoystickHandlerNode();
+namespace tcp_joystick_handler {
 
-  virtual ~TcpJoystickHandlerNode();
-};
+    class TcpJoystickHandlerNode : public rclcpp::Node {
+    private:
+
+        bool _is_connected;
+        tcp _tcp = tcp(8000);
+        std::string connected_address;
+
+        rclcpp::TimerBase::SharedPtr _pub_timer;
+
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _joystick_publisher;
+
+        std::shared_ptr<std_msgs::msg::String> _msg;
+        rclcpp::QoS _qos = rclcpp::SensorDataQoS();
+
+        void _publisher_callback();
+
+    public:
+        TCP_JOYSTICK_HANDLER_PUBLIC
+        TcpJoystickHandlerNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+
+        TCP_JOYSTICK_HANDLER_PUBLIC
+        TcpJoystickHandlerNode(const std::string &name_space,
+                               const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+
+    };
 
 }  // namespace tcp_joystick_handler
 
