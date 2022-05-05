1020d82a492f57e8e45a3fa40a2bda6cbf265242
ryuzou <ryuzot.1112@gmail.com>
2022-05-05 15:23:11 +0900

    r2アーム駆動node

--- src/r2_arm/include/r2_arm/r2_arm_node.hpp
+++ src/r2_arm/include/r2_arm/r2_arm_node.hpp
@@ -3,6 +3,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <chrono>
+#include <regex>
 
 #include "r2_arm/visibility_control.h"
 #include "utilities/json11.hpp"
@@ -12,6 +14,30 @@
 namespace r2_arm {
 
     class R2ArmNode : public rclcpp::Node {
+    private:
+
+        int arm_open_shut_movement = 0; //0:stop, 1:open, -1or2:close
+        int arm_rotating_movement = 0;  //0:stop, 1:cw, 2:ccw
+        int arm_up_down_movement = 0;   //0:stop, 1:up, -1or2:down
+        int arm_front_or_back = 0;  //0:nothing, 1:front, 2:back
+
+        bool arm_cw_flag = false;
+        bool arm_ccw_flag = false;
+        bool arm_stop_flag = false;
+
+        bool arm_front = false;
+        bool arm_back = false;
+
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _subscriber;
+        rclcpp::QoS _qos = rclcpp::ServicesQoS();
+
+        void _subscriber_callback(std_msgs::msg::String msg);
+
+        rclcpp::TimerBase::SharedPtr _pub_timer;
+        rclcpp::Publisher<socketcan_interface_msg::msg::SocketcanIF>::SharedPtr _publisher;
+        rclcpp::QoS _qos_can = rclcpp::QoS(10).keep_all();
+        void _publisher_callback();
+
     public:
         R2_ARM_PUBLIC
         R2ArmNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
